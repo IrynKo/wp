5b841a2d48f4812aaf749ca9fa43bf6162b0741e
Iryna <iramacarilla@mail.ru>
2022-06-09 14:36:38 +0200

    initial commit

---
+++ wp-content/themes/twentytwenty/template-parts/blocks/testimonial/testimonial.php
@@ -0,0 +1,13 @@
+{{-- /
+  Title: Account user-billings
+  Description: Block with user billing information
+  Category: formatting
+  Icon: admin-comments
+  Keywords: text members
+  Mode: edit
+  Align: center
+  PostTypes: page
+  SupportsAlign: false
+  SupportsMode: false
+  SupportsMultiple: false
+/ --}}
