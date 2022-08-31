ca3ea57c6b59dfee7d325154185c6674eec660f7
Iryna <iramacarilla@mail.ru>
2022-08-31 11:21:20 +0200

    -proxy

--- re/src/redux/mainPage/mainPageActions.js
+++ re/src/redux/mainPage/mainPageActions.js
@@ -4,7 +4,7 @@ import { GET_MAIN_FAILURE, GET_MAIN_REQUEST, GET_MAIN_SUCCESS } from "./mainPage
 export const getMain = () => async (dispatch) => {
     try {
       dispatch({ type: GET_MAIN_REQUEST });
-      const { data } = await axios.get("/wp-json/wp/v2/pages/24");
+      const { data } = await axios.get("https://i111191.hostde21.fornex.host/wp-json/wp/v2/pages/24");
       dispatch({ type: GET_MAIN_SUCCESS, payload: data });
     } catch (error) {
       dispatch({ type: GET_MAIN_FAILURE, payload: error.message });
